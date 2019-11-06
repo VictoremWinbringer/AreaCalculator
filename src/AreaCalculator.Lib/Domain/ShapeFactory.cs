dc15c9dc46bec6d4b912f17ecc6a4a69a6613202
VictoremWinbringer <victor@mail.ru>
2019-11-06 23:01:33 +0300

    save

---
+++ src/AreaCalculator.Lib/Domain/ShapeFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+
+namespace AreaCalculator.Lib
+{
+    public sealed class ShapeFactory
+    {
+        public Shape Create(string input)
+        {
+            return input?
+                .Split(":", System.StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.Trim().ToLower())
+                .ToArray()
+                 switch
+            {
+                var args when args.Length == 2 && args[0] == "circle" => (Shape)new Circle(new Radius(double.Parse(args[1]))),
+                var args when args.Length == 2 && args[0] == "rectangle" => CreateRectangle(args[1]),
+                _ => throw new NotImplementedException(input) { Source = typeof(ShapeFactory).FullName }
+            };
+        }
+
+        private Rectangle CreateRectangle(string input)
+        {
+            var args = input.Split("x", StringSplitOptions.RemoveEmptyEntries);
+            return new Rectangle(new Height(double.Parse(args[0])), new Width(double.Parse(args[1])));
+        }
+    }
+}
--- src/AreaCalculator/Program.cs
+++ src/AreaCalculator/Program.cs
@@ -16,9 +16,7 @@ namespace AreaCalculator
                 var circle = Console.ReadLine();
                 var rectange = Console.ReadLine();
                 var result = new[] { circle, rectange }
-                    .Select(s => s.Split(":", StringSplitOptions.RemoveEmptyEntries))
-                    .Select(sa => (sa[0], sa[1].Split("x", StringSplitOptions.RemoveEmptyEntries).Select(s => double.Parse(s)).ToArray()))
-                    .Select(sa => factory.Create(sa.Item1, sa.Item2))
+                    .Select(s => factory.Create(s))
                     .Select(s => $"Area:{s.Area.Value}, Perimeter: {s.Perimeter.Value}");
                 foreach (var r in result)
                 {
